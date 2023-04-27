31a052642852a0d42d6ebb4ffa5638f444794530
Anastasiia <avainer03@gmail.com>
2023-04-28 00:34:20 +0300

    Add the second algorithm

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-
-'''
-def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def print_matrix_turned(matrix):
+    for y in range(len(matrix[0]) - 1, -1, -1):
+        for x in range(len(matrix)):
+            print(matrix[x][y], end = "\t")
+        print("")
 
 
-class Rectangle:
-    def __init__(self, x1 = 0, y1 = 0, x2 = 0, y2 = 0):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
-
-4
-2 2 6 8
-5 4 9 10
-4 0 11 6
-8 2 12 12
-5
-2 2
-12 12
-10 4
-5 5
-2 10
-'''
+def print_matrix(matrix):
+    for x in range(len(matrix)):
+        for y in range(len(matrix[0])):
+            print(matrix[x][y], end = "\t")
+        print("")
 
 
 def first_algorithm(rectangles, points):
@@ -45,21 +28,35 @@ def first_algorithm(rectangles, points):
     print("\n")
 
 
-def binarysearch(coord_compressed_keys, target, low, high):
+def binarysearch_v1(coord_compressed_keys, target, low, high):
     if high >= low:
         mid = low + (high - low)//2
         if coord_compressed_keys[mid] == target:
             return coord_compressed_keys[mid]
         elif coord_compressed_keys[mid] > target:
-            return binarysearch(coord_compressed_keys, target, low, mid-1)
+            return binarysearch_v1(coord_compressed_keys, target, low, mid-1)
         else:
-            return binarysearch(coord_compressed_keys, target, mid + 1, high)
+            return binarysearch_v1(coord_compressed_keys, target, mid + 1, high)
     else:
         elem = max(high,0)
         return coord_compressed_keys[elem]
 
 
-def second_algorithm(rectangles, points):
+def binary_search(coord_compressed, target, low, high):
+    if high >= low:
+        mid = low + (high - low)//2
+        if coord_compressed[mid] == target:
+            return mid
+        elif coord_compressed[mid] > target:
+            return binary_search(coord_compressed, target, low, mid-1)
+        else:
+            return binary_search(coord_compressed, target, mid + 1, high)
+    else:
+        elem = max(high,0)
+        return elem
+
+
+def second_algorithm_v1(rectangles, points):
     # creating the dictionaries: original coordinates -> compressed coordinates
     x_compressed = {}
     y_compressed = {}
@@ -98,10 +95,15 @@ def second_algorithm(rectangles, points):
         for x in range(x_compressed[rectangle[0]], x_compressed[rectangle[2]]):
             for y in range(y_compressed[rectangle[1]], y_compressed[rectangle[3]]):
                 matrix[x][y] += 1
+        print_matrix_turned(matrix)
+        print("\n")
 
-    for k in range(len(x_compressed)):
-        print(matrix[k])
+    # print_matrix(matrix)
+    # print("\n")
+    print_matrix_turned(matrix)
 
+
+'''
     for point in points:
         coord_compressed_x = list(x_compressed.keys())
         coord_compressed_y = list(y_compressed.keys())
@@ -109,6 +111,44 @@ def second_algorithm(rectangles, points):
         if not (point[0] > coord_compressed_x[-1] or point[1] > coord_compressed_y[-1]):
             x = binarysearch(coord_compressed_x, point[0], 0, len(coord_compressed_x))
             y = binarysearch(coord_compressed_y, point[1], 0, len(coord_compressed_y))
+            intersections = matrix[x][y]
+        print(intersections, end = " ")
+'''
+
+
+def second_algorithm(rectangles, points):
+    # creating the sets
+    x_compressed = set()
+    y_compressed = set()
+
+    for rectangle in rectangles:
+        x_compressed.add(rectangle[0])
+        x_compressed.add(rectangle[2])
+        y_compressed.add(rectangle[1])
+        y_compressed.add(rectangle[3])
+
+    # sorting the sets that were turned into lists
+    x_compressed = sorted(x_compressed)
+    y_compressed = sorted(y_compressed)
+
+    matrix = [0] * len(x_compressed)
+    for j in range(len(x_compressed)):
+        matrix[j] = [0] * len(y_compressed)
+
+    for rectangle in rectangles:
+        for x in range(x_compressed.index(rectangle[0]), x_compressed.index(rectangle[2])):
+            for y in range(y_compressed.index(rectangle[1]), y_compressed.index(rectangle[3])):
+                matrix[x][y] += 1
+        # print_matrix_turned(matrix)
+        # print("\n")
+
+    # print("Final matrix")
+    # print_matrix_turned(matrix)
+    for point in points:
+        intersections = 0
+        if x_compressed[0] <= point[0] <= x_compressed[-1] and y_compressed[0] <= point[1] <= y_compressed[-1]:
+            x = binary_search(x_compressed, point[0], 0, len(x_compressed))
+            y = binary_search(y_compressed, point[1], 0, len(y_compressed))
             intersections = matrix[x][y]
         print(intersections, end = " ")
 
@@ -127,3 +167,25 @@ if __name__ == '__main__':
 
     first_algorithm(Rectangles, Points)
     second_algorithm(Rectangles, Points)
+
+
+'''
+    class Rectangle:
+        def __init__(self, x1 = 0, y1 = 0, x2 = 0, y2 = 0):
+            self.x1 = x1
+            self.y1 = y1
+            self.x2 = x2
+            self.y2 = y2
+
+4
+2 2 6 8
+5 4 9 10
+4 0 11 6
+8 2 12 12
+5
+2 2
+12 12
+10 4
+5 5
+2 10
+'''
